40db537ed04e9ca3eeeb492c6e7a328b077af215
Keith Lockey <lockey@gmail.com>
2017-07-19 11:03:26 -0400

    first submittal

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+      <div id="container">
+        <header>
+            <h1> Shout out!! </h1>
+        </header >
+        <div id="shouts">
+          <ul>
+              <li class='shout'> <span> 10:15 pm </span>Brad: what are you up to? </li>
+              <li class='shout'> <span> 10:15 pm </span>Brad: what are you up to? </li>
+              <li class='shout'> <span> 10:15 pm </span>Brad: what are you up to? </li>
+              <li class='shout'> <span> 10:15 pm </span>Brad: what are you up to? </li>
+              <li class='shout'> <span> 10:15 pm </span>Brad: what are you up to? </li>
+              <li class='shout'> <span> 10:15 pm </span>Brad: what are you up to? </li>
+          </ul>
+      </div>
+        <div id="input">
+          <form method="post" action="process.php">
+            <input type="text" name="user" placeholder="Enter your name" />
+            <input type="text" name="message" placeholder="Message">
+          </br>
+          <input type="submit" name="submit" value="shout it out" class="shout-btn" />
+        </div>
+    </div>
+  </body>
+</html>
+
+<?php
+
+
+?>
